d515a20ab233abbb175705b913a02f79b2d4f767
probison <parker.robison@hotmail.com>
2020-05-23 09:58:02 -0600

    added divs, classes and ids to organize the code

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+
+
+
+// inside which HTML element do we put the Javascript?
+// <script>
+// <javascript>
+// <scripting>
+// <js>
+
+// How does a for loop start?
+// for (i = 0; i < 5; i++)
+// for (i < 5; i++)
+// for (i = 0; i < 5)
+// for i = 1 to 5
