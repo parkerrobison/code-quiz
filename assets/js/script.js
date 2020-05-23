e9c9a273fb6e758f59e67e452bf008beef71677b
probison <parker.robison@hotmail.com>
2020-05-23 12:38:12 -0600

    started js variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,37 @@
+var qContainerEl = document.querySelector("#q-container");
+var startButtonEl = document.querySelector("#start-btn");
+var mainTextEl = document.querySelector("#main-text");
+var subTextEl = document.querySelector("#sub-text");
+var countdownTimer = document.querySelector("#timer");
+var answerButtons = document.querySelector("#answer-buttons");
+
+var questions = [
+    {q: 'How does a for loop start?', a: [
+        {text:"1. for (i = 0; i < 5; i++)"},
+        {text:"2. for for (i < 5; i++)"},
+        {text:"3. for (i = 0; i < 5)"},
+        {text:"4. for i = 1 to 5"}
+    
+    ]}
+]
+
+// start screen
+
+function startQuiz() {
+console.log('Start!')
+setNextQuestion()
+}
+// questions screen
+
+function countdownStart(){
+
+}
+function setNextQuestion() {
+
+}
+
+// end screen
+
 
 
 
@@ -7,8 +41,5 @@
 // <scripting>
 // <js>
 
-// How does a for loop start?
-// for (i = 0; i < 5; i++)
-// for (i < 5; i++)
-// for (i = 0; i < 5)
-// for i = 1 to 5
+startButtonEl.addEventListener('click', startQuiz)
+
